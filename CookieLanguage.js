dae3905147f784279e69f28d084505f7d9bf7e10
jojoxd <joeyvos@outlook.com>
2014-06-06 15:53:07 +0200

    Initial commit

---
+++ CookieLanguage.js
@@ -0,0 +1,68 @@
+var CookieLang = {
+	init: function(){
+		// add CookieLang to game's init
+		Game.customInit.push();
+	},
+	
+	core: {
+		loadTranslation: function(lang){
+			// copied from Game.loadMod
+			var js=document.createElement('script');
+			var id=url.split('/');id=id[id.length-1].split('.')[0];
+			js.setAttribute('type','text/javascript');
+			js.setAttribute('id','modscript_' + id);
+			js.setAttribute('src', url);
+			document.head.appendChild(js);
+			console.log('Loaded Language file ' + url + ', ' + id + '.');
+		}
+		setTranslation: function(){
+		},
+		
+		setNumberFormatters: function(){
+			numberformatters = 
+			[
+				rawFormatter,
+				formatEveryThirdPower([
+					// would've used long, but it's a definition
+					'',
+					translation.million.big,
+					translation.billion.big,
+					translation.trillion.big,
+					translation.quadrillion.big,
+					translation.quintillion.big,
+					translation.sextillion.big,
+					translation.septillion.big,
+					translation.octillion.big,
+					translation.nontillion.big,
+					translation.decillion.big,
+				]),
+				
+				formatEveryThirdPower([
+					'',
+					translation.million.small,
+					translation.billion.small,
+					translation.trillion.small,
+					translation.quadrillion.small,
+					translation.quintillion.small,
+					translation.sextillion.small,
+					translation.septillion.small,
+					translation.octillion.small,
+					translation.nontillion.small,
+					translation.decillion.small,
+				]),
+			];
+		},
+	},
+	
+	_core: {
+		
+	},
+	
+	vars: {
+		
+	},
+	
+	_vars: {
+		
+	},
+};
